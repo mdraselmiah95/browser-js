09b72c053c4474af0bfa07dfb9287617ceeeff16
rasel <russelroy15@gmail.com>
2021-09-04 14:35:47 +0600

    useing setTime out function

--- fetch.js
+++ fetch.js
@@ -1,4 +1,8 @@
 console.log(1111111);
 console.log(2222222);
+// setTimeout(() => {
+//   console.log("This is a setTimeOut function");
+// }, 3000);
+setTimeout(() => console.log("This is set time out example."), 4000);
 console.log(3333333);
 console.log(4444444);
