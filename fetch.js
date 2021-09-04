12b6e03da95ad7d64ae0dfb2669624b01dc43eec
rasel <russelroy15@gmail.com>
2021-09-04 14:38:49 +0600

    add new code

--- fetch.js
+++ fetch.js
@@ -9,3 +9,6 @@ fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then((json) => console.log(json));
 console.log(3333333);
 console.log(4444444);
+for (let i = 0; i < 200; i++) {
+  console.log(i);
+}
