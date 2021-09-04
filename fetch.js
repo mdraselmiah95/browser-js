9aeb005d46e6316bed5c572e34d326907d6b4e52
rasel <russelroy15@gmail.com>
2021-09-04 14:36:33 +0600

    useing setTime out function

--- fetch.js
+++ fetch.js
@@ -4,5 +4,8 @@ console.log(2222222);
 //   console.log("This is a setTimeOut function");
 // }, 3000);
 setTimeout(() => console.log("This is set time out example."), 4000);
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 console.log(3333333);
 console.log(4444444);
