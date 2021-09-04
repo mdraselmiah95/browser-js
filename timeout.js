dbc37d9fe18ca23bd1fc214da917830f3b0613ea
rasel <russelroy15@gmail.com>
2021-09-04 14:26:59 +0600

    new code  added

---
+++ timeout.js
@@ -0,0 +1,9 @@
+function doSomething() {
+  console.log("I doing coding(javaScript).");
+}
+
+console.log("First:I am the First.");
+console.log("Second:I am the Second.");
+console.log("Third:I am the Third.");
+setTimeout(doSomething);
+console.log("Fourth:I am the Fourth.");
